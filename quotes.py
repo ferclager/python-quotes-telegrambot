549c663220f6e79a70aeb3544add4773010e828a
FeR Clager <ferclager@users.noreply.github.com>
2023-09-22 13:48:57 +0200

    Create quotes.py
    First version

---
+++ quotes.py
@@ -0,0 +1,36 @@
+import requests
+import urllib
+
+TELEGRAM_BOT_TOKEN = "YOUR_TOKEN"
+TELEGRAM_CHAT_ID = "YOUR_CHAT_ID"
+
+URL_QUOTE = "https://quotes.rest/qod"
+HEADERS = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'X-TheySaidSo-Api-Secret': 'api_key'
+}
+
+def getQuote():
+  r = requests.get(url = URL_QUOTE, headers = HEADERS)
+  data = r.json()
+  message = ''
+  if 'contents' in data:
+    quote = data['contents']['quotes'][0]['quote']
+    author = data['contents']['quotes'][0]['author']
+    permalink = data['contents']['quotes'][0]['permalink']
+    message = quote + ' (' + author + ')'
+  elif 'error' in data:
+    print("Error: " + data['error']['message'])
+  return message
+
+def sendMessage(query):
+    r = requests.get(url = "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendMessage?chat_id=" + TELEGRAM_CHAT_ID + "&text=" + urllib.parse.quote(query), headers = HEADERS)
+
+def main():
+  quote = getQuote()
+  if (quote != ''):
+    sendMessage()
+
+if __name__ == "__main__":
+  main()
