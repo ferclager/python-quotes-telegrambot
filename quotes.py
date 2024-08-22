a2b0e9691f212bd871df1ba85a9a8ed5cae4428f
ferclager <17533449+ferclager@users.noreply.github.com>
2024-08-22 15:19:36 -0600

    add log

--- quotes.py
+++ quotes.py
@@ -9,6 +9,8 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 API_KEY_THEYSAIDSO = os.getenv("API_KEY_THEYSAIDSO")
 
+print("DELETEME " + TELEGRAM_CHAT_ID)
+
 URL_QUOTE = "https://quotes.rest/qod"
 HEADERS = {
   'Accept': 'application/json',
